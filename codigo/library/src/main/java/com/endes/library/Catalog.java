801632258989af82139ff0f522e2e48322441c51
jescdom238 <jescdom238@g.educaand.es>
2024-04-23 21:12:30 +0200

    Creada clase Catalog e implementadas las interfaces v2

--- codigo/library/src/main/java/com/endes/library/Catalog.java
+++ codigo/library/src/main/java/com/endes/library/Catalog.java
@@ -3,6 +3,43 @@ package com.endes.library;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Catalog {
-	Set<String> records= new HashSet<>();
+public class Catalog implements Manage,Search {
+	
+	Set<BookItem> records= new HashSet<>();
+
+	@Override
+	public void createBookItem(BookItem bookItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBookItem(BookItem bookItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBookItem(String ISBN) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BookItem getBookItem(String ISBN) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BookItem searchByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BookItem searchByAuthor(String author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
